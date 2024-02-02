f94e6006e79b07759cf9844f0cec4d3930d0a931
yasmin tsarwah <95679916+yasmintsarwah@users.noreply.github.com>
2024-02-02 13:12:16 +0700

    11
    bagian 11

---
+++ 11/src/index.js
@@ -0,0 +1,92 @@
+import { initializeApp } from 'firebase/app'
+import {
+  getFirestore, collection, onSnapshot,
+  addDoc, deleteDoc, doc,
+  query, where,
+  orderBy, serverTimestamp,
+  updateDoc
+} from 'firebase/firestore'
+import {
+  getAuth
+} from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDmXgb_58lO7aK_ujN37pGlNxzWGEU0YpI",
+  authDomain: "fb9-sandbox.firebaseapp.com",
+  projectId: "fb9-sandbox",
+  storageBucket: "fb9-sandbox.appspot.com",
+  messagingSenderId: "867529587246",
+  appId: "1:867529587246:web:dc754ab7840c737f47bdbf"
+}
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+const auth = getAuth()
+
+// collection ref
+const colRef = collection(db, 'books')
+
+// queries
+const q = query(colRef, where("author", "==", "patrick rothfuss"), orderBy('createdAt'))
+
+// realtime collection data
+onSnapshot(q, (snapshot) => {
+  let books = []
+  snapshot.docs.forEach(doc => {
+    books.push({ ...doc.data(), id: doc.id })
+  })
+  console.log(books)
+})
+
+// adding docs
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+    createdAt: serverTimestamp()
+  })
+  .then(() => {
+    addBookForm.reset()
+  })
+})
+
+// deleting docs
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const docRef = doc(db, 'books', deleteBookForm.id.value)
+
+  deleteDoc(docRef)
+    .then(() => {
+      deleteBookForm.reset()
+    })
+})
+
+// fetching a single document (& realtime)
+const docRef = doc(db, 'books', 'gGu4P9x0ZHK9SspA1d9j')
+
+onSnapshot(docRef, (doc) => {
+  console.log(doc.data(), doc.id)
+})
+
+// updating a document
+const updateForm = document.querySelector('.update')
+updateForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  let docRef = doc(db, 'books', updateForm.id.value)
+
+  updateDoc(docRef, {
+    title: 'updated title'
+  })
+  .then(() => {
+    updateForm.reset()
+  })
+})
